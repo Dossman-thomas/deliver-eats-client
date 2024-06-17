50a0ef3e8540e8e7983917b9f1ae38ea5a6278a0
Dossman-thomas <tdossman.development@gmail.com>
2024-06-17 15:23:42 -0400

    error message dealt with

--- src/forms/user-profile-form/manage-restaurant-form/MenuItemInput.tsx
+++ src/forms/user-profile-form/manage-restaurant-form/MenuItemInput.tsx
@@ -7,7 +7,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { useFormContext } from "react-hook-form";
 
 type Props = {
