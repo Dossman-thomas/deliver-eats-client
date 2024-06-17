69f7d5acaff2ea264ee05c5ca0011003807c4244
Dossman-thomas <tdossman.development@gmail.com>
2024-06-17 14:08:26 -0400

    Menu item input section of manage restaurant form created and functional in browser.  User able to add and remove menu items.

---
+++ src/forms/user-profile-form/manage-restaurant-form/MenuItemInput.tsx
@@ -0,0 +1,84 @@
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useFormContext } from "react-hook-form";
+
+type Props = {
+  index: number;
+  removeMenuItem: () => void;
+};
+
+const MenuItemInput = ({ index, removeMenuItem }: Props) => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="flex flex-col md:items-end gap-3 md:flex-row">
+      <FormField
+        control={control}
+        name={`menuItems.${index}.name`}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="flex items-center gap-1">
+              Name <FormMessage />
+            </FormLabel>
+            <FormControl>
+              <Input
+                {...field}
+                placeholder="Cheeseburger"
+                className="bg-white"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name={`menuItems.${index}.price`}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="flex items-center gap-1">
+              Price ($) <FormMessage />
+            </FormLabel>
+            <FormControl>
+              <Input {...field} placeholder="8.00" className="bg-white" />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={control}
+        name={`menuItems.${index}.description`}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className="flex items-center gap-1">
+              Description <FormMessage />
+            </FormLabel>
+            <FormControl>
+              <Input
+                {...field}
+                placeholder="Describe the menu item"
+                className="bg-white flex-1 rounded-md"
+              />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+      <Button
+        type="button"
+        onClick={removeMenuItem}
+        className="bg-red-500 max-h-fit"
+      >
+        Remove
+      </Button>
+    </div>
+  );
+};
+
+export default MenuItemInput;
