7a4eb683919a6387f234a8a2781d47913a8e14ac
Dossman-thomas <tdossman.development@gmail.com>
2024-06-20 15:22:55 -0400

    getMyrestaurant API request created

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -1,16 +1,48 @@
 import { Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyRestaurantRequest = async (): Promise<Restaurant> => {
+    // defining the type of the response
+
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      // make a GET request to the API to fetch the authenticated user's restaurant
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch restaurant"); // throw an error if the response is not ok
+    }
+
+    return response.json(); // return the response as JSON
+  };
+
+  const { data: restaurant, isLoading } = useQuery(
+    "fetchMyRestaurant",
+    getMyRestaurantRequest
+  );
+
+  return { restaurant, isLoading };
+  
+};
 
 export const useCreateMyRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
 
   const createMyRestaurantRequest = async (
     restaurantFormData: FormData
-  ): Promise<Restaurant> => { // defining the type of the response
+  ): Promise<Restaurant> => {
+    // defining the type of the response
     const accessToken = await getAccessTokenSilently();
 
     const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
