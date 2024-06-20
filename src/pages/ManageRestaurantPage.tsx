78464acd3ac75c3893ea678107e3e359b849bc3d
Dossman-thomas <tdossman.development@gmail.com>
2024-06-20 15:38:45 -0400

    import error in ManageRestaurantPage handled.

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,4 +1,4 @@
-import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
