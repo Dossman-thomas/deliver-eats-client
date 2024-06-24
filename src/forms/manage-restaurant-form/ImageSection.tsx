ef1e809a47e5104cdd9530b9607119c8b5bf7b6a
Dossman-thomas <tdossman.development@gmail.com>
2024-06-24 10:05:43 -0400

    aspect-ratio shadcn ui component added so manage-restaurant form displays an existing Image url if it's been uploaded by the user. Display is responsive.

--- src/forms/manage-restaurant-form/ImageSection.tsx
+++ src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,3 +1,4 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import {
   FormControl,
   FormDescription,
@@ -9,20 +10,29 @@ import { Input } from "@/components/ui/input";
 import { useFormContext } from "react-hook-form";
 
 const ImageSection = () => {
-  const { control } = useFormContext();
+  const { control, watch } = useFormContext();
+
+  const existingImageUrl = watch("imageUrl");
 
   return (
     <div className="space-y-2">
       <div>
-        <h2 className="text-2xl font-bold">
-            Image
-        </h2>
+        <h2 className="text-2xl font-bold">Image</h2>
         <FormDescription>
           Add an image that will be displayed on your restaurant listing in the
           search results. Adding a new image will overwrite the existing one.
         </FormDescription>
       </div>
-      <div className="flex flex-col gap-8 w-[50%]">
+      <div className="flex flex-col gap-8 md:w-[50%]">
+        {existingImageUrl && (
+          <AspectRatio ratio={16 / 9}>
+            <img
+              src={existingImageUrl}
+              alt="Existing restaurant image"
+              className="rounded-md object-cover h-full w-full"
+            />
+          </AspectRatio>
+        )}
         <FormField
           control={control}
           name="imageFile"
@@ -34,7 +44,8 @@ const ImageSection = () => {
                   type="file"
                   accept=".jpg, .jpeg, .png, .pdf"
                   onChange={(event) =>
-                    field.onChange( // updating the field value with the selected file
+                    field.onChange(
+                      // updating the field value with the selected file
                       event.target.files ? event.target.files[0] : null
                     )
                   }
