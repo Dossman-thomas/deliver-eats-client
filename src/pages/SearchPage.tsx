f0365616199adc15845ea2f86d2e2b27149df121
Dossman-thomas <tdossman.development@gmail.com>
2024-07-17 13:47:13 -0400

    RestaurantSearchResponse types created and  added to restaurantApi

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,8 +1,10 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams(); 
-
+  const { results, isLoading } = useSearchRestaurants(city);
+  
   return <span>User Searched for {city}</span>
 }
 
